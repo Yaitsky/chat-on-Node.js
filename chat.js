665b124f821db4c88e7771b9f7905b65c0d965e4
Igor Yaitsky <yaickiu@gmail.com>
2017-03-22 22:19:20 +0300

    first commit

---
+++ chat.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+var port = 8000;
+
+logger.debug('Script started');
+
+server.listen(port);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+    var name = "ID" + (socket.id).toString().substr(1,4);
+
+    socket.broadcast.emit('newUser', name);
+    socket.emit('userName', name);
+    logger.info(name + ' connected to chat');
+
+    socket.on('message', function (message) {
+        logger.info(message);
+        io.emit('messageToClients', message, name);
+    });
+
+    socket.on('disconnect', function (disc) {
+        logger.info(name + ' disc');
+        io.emit('disconnect', name);
+    })
+});
---
+++ public/main.js
@@ -0,0 +1,47 @@
+var port = 8000;
+var socket = io.connect('http://localhost:' + port);
+
+var chatList = document.querySelector('#chatList');
+var sendButton = document.querySelector('#send');
+var textInput = document.querySelector('#text');
+
+socket.on('userName', function (userName) {
+    var newItem = document.createElement('li');
+    newItem.innerText = "Your username is " + userName;
+    chatList.appendChild(newItem);
+});
+
+socket.on('newUser', function (userName) {
+    var newItem = document.createElement('li');
+    newItem.innerText = "New user! His name is " + userName;
+    chatList.appendChild(newItem);
+});
+
+sendButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    var message = textInput.value;
+
+    socket.emit('message', message);
+    textInput.value = '';
+});
+
+socket.on('messageToClients', function (message, name) {
+    var newItem = document.createElement('li');
+    newItem.innerText = name + ": ---- " + message;
+
+    chatList.appendChild(newItem);
+});
+
+socket.on('disconnect', function (name) {
+    var newItem = document.createElement('li');
+    newItem.innerText = name + ' disconnected';
+
+    chatList.appendChild(newItem);
+})
+
+
+
+
+
+
+
